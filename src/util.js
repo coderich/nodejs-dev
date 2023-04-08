530661ae7b79d83975aedcb0fdf2fe839a09a15a
Richard Livolsi <richard.livolsi@gmail.com>
2023-04-08 13:47:11 -0400

    try try again

--- src/util.js
+++ src/util.js
@@ -12,8 +12,11 @@ exports.AppRootPackage = require(`${AppRootPath}/package.json`);
 exports.AppRootPath = AppRootPath;
 
 exports.shellCommand = (cmd, ...args) => {
-  const child = ChildProcess.spawnSync(cmd, args.flat(), { shell: true, encoding: 'utf8' });
-  if (child.error) throw new Error(child.error);
-  if (child.stderr.length) throw new Error(child.stderr);
-  return child.stdout.trim();
+  let stdin, stdout, stderr;
+  ChildProcess.spawnSync(cmd, args.flat(), { shell: true, encoding: 'utf8', stdio: [stdin, stdout, stderr] });
+  if (stderr?.length) throw new Error(stderr);
+  return stdout.trim();
+  // if (child.error) throw new Error(child.error);
+  // if (child.stderr.length) throw new Error(child.stderr);
+  // return child.stdout.trim();
 };
